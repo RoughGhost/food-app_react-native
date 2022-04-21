c12bb23cdfbaf35b5e8b42c3b987c0fa9b9c3412
RoughGhost <stephendlouis6@gmail.com>
2022-04-21 23:13:42 +0100

    updated TextIconButton

---
+++ React-Native/food-app/components/TextButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { FONTS, COLORS } from "../constants";
+
+const TextButton = ({ label, labelStyle, buttonContainerStyle, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: COLORS.primary,
+        ...buttonContainerStyle,
+      }}
+      onPress={onPress}
+    >
+      <Text style={{ color: COLORS.white, ...FONTS.h3, ...labelStyle }}>
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TextButton;
